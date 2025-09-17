3ec9b32a853768ce237fc2a687f391c2ede33bc9
Michele Wiseman <33985124+mswiseman@users.noreply.github.com>
2025-09-17 12:58:33 -0700

    Update hmm.sh

--- hmm.sh
+++ hmm.sh
@@ -21,7 +21,7 @@ if [ ! -s PF03094.hmm ]; then
   wget -q https://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.hmm.gz
   gunzip -c Pfam-A.hmm.gz > Pfam-A.hmm
   hmmfetch --index Pfam-A.hmm                              # build SSI for text HMM
-  hmmfetch Pfam-A.hmm PF03094 > PF03094.hmm || hmmfetch Pfam-A.hmm Mlo > PF03094.hmm
+  hmmfetch Pfam-A.hmm PF03094.20 > PF03094.hmm || hmmfetch Pfam-A.hmm Mlo > PF03094.hmm
 fi
 [ -s PF03094.hmm ] || { echo "[ERROR] PF03094.hmm not created"; exit 1; }
 
